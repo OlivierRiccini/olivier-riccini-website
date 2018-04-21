0b536c61c2fbce60e8cfe2e19b52a3cf5004e82c
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-20 20:07:58 -0400

    just one thing to fix about menu navigation

--- app/javascript/components/_custom_fullpage.js
+++ app/javascript/components/_custom_fullpage.js
@@ -32,6 +32,9 @@ const homeP = document.getElementById('home-p');
 const projectsP = document.getElementById('projects-p');
 const contactP = document.getElementById('contact-p');
 
+const spanHome = document.querySelector('#home.span1');
+const spanProjects = document.querySelector('#projects.span1');
+const spanContact = document.querySelector('#contact.span1');
 
 function slideLeft(item) {
   item.style.right = 0;
@@ -42,7 +45,28 @@ function slideRight(item) {
 }
 
 function display(item) {
-  item.classList.add('display-class');
+  if ( item === homeP ) {
+    homeP.classList.add('display-class');
+    spanHome.classList.add('span-active');
+    projectsP.classList.remove('display-class');
+    contactP.classList.remove('display-class');
+    spanProjects.classList.remove('span-active');
+    spanContact.classList.remove('span-active');
+  } else if ( item === projectsP ) {
+    homeP.classList.remove('display-class');
+    spanHome.classList.remove('span-active');
+    projectsP.classList.add('display-class');
+    contactP.classList.remove('display-class');
+    spanProjects.classList.add('span-active');
+    spanContact.classList.remove('span-active');
+  } else {
+    homeP.classList.remove('display-class');
+    spanHome.classList.remove('span-active');
+    projectsP.classList.remove('display-class');
+    contactP.classList.add('display-class');
+    spanProjects.classList.remove('span-active');
+    spanContact.classList.add('span-active');
+  }
 }
 
 home.addEventListener('mouseover', function() { slideLeft(homeP) });
@@ -57,10 +81,89 @@ home.addEventListener('click', function() { display(homeP) });
 projects.addEventListener('click', function() { display(projectsP) });
 contact.addEventListener('click', function() { display(contactP) });
 
+/* Arrow to top 0 */
+$(document).ready(function(){
+  $('.arrow-top-0').click(function(){
+    $.fn.fullpage.moveTo(1);
+    return false;
+  });
+});
+
+/* Arrow pulse bottom */
+const arrowDownCircle = document.querySelectorAll('.arrow-down-circle');
+
+function globalArrowDownAnimation() {
+  arrowDownCircle.forEach(function(arrow) {
+    const arrowDown = arrow.querySelectorAll('.arrow-down');
+    let intervalArrowDown = null;
+
+    function defIntervalArrowDown() {
+      intervalArrowDown = setInterval(arrwoDownAnimation, 50);
+    }
+
+    let arrowDownIndex = 5;
+    let arrowDownColorIndex = 0;
+
+    function arrwoDownAnimation() {
+      arrowDownIndex += 1;
+      arrowDownColorIndex += 0.05;
+      arrowDown[0].style.top = `${arrowDownIndex}px`;
+      arrowDown[0].style.borderColor = `rgba(0,0,0,${arrowDownColorIndex})`;
+      if (arrowDownColorIndex >= 1) {
+        arrowDownColorIndex = 0;
+      }
+      if (arrowDownIndex >= 20) {
+        arrowDownIndex = 5;
+      }
+    }
+
+    /* calling scroll down function using index defining scrolling height */
+    arrow.addEventListener("click", function(){$.fn.fullpage.moveSectionDown()});
+
+    if (arrow.dataset.arrowDown == nbProjects + 1) {
+      arrow.style.display = 'none';
+    }
+
+  defIntervalArrowDown();
+
+  });
+}
+globalArrowDownAnimation();
 
 
+/* Arrow pulse up */
+const arrowUpCircle = document.querySelectorAll('.arrow-up-circle');
 
+function globalArrowUpAnimation() {
+  arrowUpCircle.forEach(function(arrow) {
+    const arrowUp = arrow.querySelectorAll('.arrow-up');
+    let intervalArrowUp = null;
 
+    function defIntervalArrowUp() {
+      intervalArrowUp = setInterval(arrwoUpAnimation, 50);
+    }
 
+    let arrowUpIndex = 5;
+    let arrowUpColorIndex = 0;
 
+    function arrwoUpAnimation() {
+      arrowUpIndex += 1;
+      arrowUpColorIndex += 0.05;
+      arrowUp[0].style.top = `${arrowUpIndex}px`;
+      arrowUp[0].style.borderColor = `rgba(0,0,0,${arrowUpColorIndex})`;
+      if (arrowUpColorIndex >= 1) {
+        arrowUpColorIndex = 0;
+      }
+      if (arrowUpIndex >= 20) {
+        arrowUpIndex = 5;
+      }
+    }
 
+    /* calling scroll down function using index defining scrolling height */
+    arrow.addEventListener("click", function(){$.fn.fullpage.moveSectionUp();});
+
+  defIntervalArrowUp();
+
+  });
+}
+globalArrowUpAnimation();
