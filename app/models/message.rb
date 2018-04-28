097cba9b0ea18aa9a00d26d048f704aab7b02ad4
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-28 16:43:06 -0400

    doing some ajax for form validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,12 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one :appointment
 
-  # validates :email, presence: true
-  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  # validates :object, presence: true
-  # validates :content, length: { minimum: 20, maximum: 500 }
-
-  # if self.appointment
-  #     validates :phone_number, presence: true, format: { with: /[\+\d]{9,}/, message: "should only contain digits and '+'" }
-  # end
+  validates :content, length: { minimum: 20, maximum: 500 }
 end
