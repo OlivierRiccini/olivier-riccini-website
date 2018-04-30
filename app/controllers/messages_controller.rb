6654d79c2ceae2b7d1bec8e15e619ecbee17a506
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-30 17:44:23 -0400

    params for checkboxes to fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,63 +5,34 @@ class MessagesController < ApplicationController
   def show
   end
 
-  # def create
-  #   @user = User.create(email: message_params[:email],
-  #                       first_name: message_params[:first_name],
-  #                       last_name: message_params[:last_name],
-  #                       company: message_params[:company],
-  #                       phone_number: message_params[:phone_number],
-  #                       password: "123456")
-  #   @message = Message.new(message_params)
-  #   @message[:user_id] = @user.id
-  #   authorize @message
-  #   @appointment = Appointment.new
-
-  #   if @message.save
-  #     @appointment[:message_id] = @message.id
-  #     @appointment[:appointment_type] = params[:message][:appointment][:appointment_type]
-  #     @appointment[:date] = params[:message][:appointment][:date]
-  #     @appointment.save
-  #     redirect_to root_path
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
-
-  # def create
-  #   @user = User.new(first_name: params[:first_name], last_name: params[:last_name], company: params[:company],
-  #                    email: params[:email], phone_number: params[:phone_number], password: "123456")
-
-  #   if @user.save
-  #     @message = Message.new(message_params)
-  #     @message[:user_id] = @user.id
-  #     authorize @message
-  #     if @message.save
-  #        @appointment = Appointment.new(appointment_params)
-
-  #       @appointment[:message_id] = @message.id
-  #       if @appointment.save
-  #           redirect_to root_path
-  #       else
-  #          redirect_to root_path
-  #       end
-  #     end
-  #   end
-  # end
-
-
   def create
-    @user = User.new(password: "123456")
-    @user[:first_name] = params[:message][:user][:first_name]
-    @user[:last_name] = params[:message][:user][:last_name]
-    @user[:company] = params[:message][:user][:company]
-    @user[:email] = params[:message][:user][:email]
-    @user[:phone_number] = params[:message][:user][:phone_number]
-    # if @user.save
-    @user.save
-
+    if User.where(email: params[:message][:user][:email]).exists?
+      @user = User.where(email: params[:message][:user][:email]).take
+    else
+      @user = User.new(password: "123456")
+      @user[:first_name] = params[:message][:user][:first_name]
+      @user[:last_name] = params[:message][:user][:last_name]
+      @user[:company] = params[:message][:user][:company]
+      @user[:email] = params[:message][:user][:email]
+      @user[:phone_number] = params[:message][:user][:phone_number]
+      @user.save
+    end
     @message = Message.new(message_params)
     @message[:user_id] = @user.id
+
+    check_boxes_params = []
+
+    if params[:message][:object].include? "appointment"
+      check_boxes_params << "appointment"
+    elsif params[:message][:object].include? "technical question"
+      check_boxes_params << "technical question"
+    elsif params[:message][:object].include? "about my projects"
+      check_boxes_params << "about my projects"
+    elsif params[:message][:object].include? "other"
+      check_boxes_params << "other"
+    end
+
+    @message[:object] = check_boxes_params
     authorize @message
 
     @appointment = Appointment.new
@@ -75,39 +46,11 @@ class MessagesController < ApplicationController
 
         format.js
       else
-        format.html { render "/"}
+        format.html { render "/" }
         format.js
       end
     end
-    # end
   end
-
-  # def create
-  #   @user = User.new(first_name: params[:first_name], last_name: params[:last_name], company: params[:company],
-  #                    email: params[:email], phone_number: params[:phone_number], password: "123456")
-
-  #   if @user.save
-  #     @message = Message.new(message_params)
-  #     @message[:user_id] = @user.id
-  #     authorize @message
-  #     if @message.save
-  #        @appointment = Appointment.new(appointment_params)
-
-  #       @appointment[:message_id] = @message.id
-  #       if @appointment.save
-  #           redirect_to root_path
-  #       else
-  #          redirect_to root_path
-  #       end
-  #     end
-  #   end
-  # end
-
-
-
-
-
-
 
   def destroy
     @message.destroy
@@ -117,26 +60,12 @@ class MessagesController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit( :first_name, :last_name, :company,
-                                  :email, :phone_number)
-  end
-
-  def appointment_params
-    params.require(:appointment).permit( :appointment_type, :date)
-  end
-
   def find_message
     @message = Message.find(params[:id])
   end
 
-  # def message_params
-  #   params.require(:message).permit( :first_name, :last_name, :company,
-  #                                    :email, :object, :phone_number, :content)
-  # end
-
   def message_params
-    params.require(:message).permit( :object, :content)
+    params.require(:message).permit(:content)
   end
 
 end
